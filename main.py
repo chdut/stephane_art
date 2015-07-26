5726124011aa072b68c91d31acae27f7f13e7f6d
Stephane <chdut.st@gmail.com>
2015-07-26 22:11:25 +0200

    Remplace les variables global par memcach

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import webapp2
 import jinja2
 import os
 import random
+from datetime import date
 
 from google.appengine.ext import ndb
+from google.appengine.api import memcache
 from google.appengine.api import images
 from google.appengine.api import users
 from google.appengine.ext import blobstore
@@ -11,15 +13,10 @@ from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.api.images import get_serving_url
 from google.appengine.datastore.datastore_query import Cursor
 
-
 template_dir = os.path.join(os.path.dirname(__file__), 'template')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
 
-LIST_TAGS = ["All"]
-LIST_TAG_UPDATED = False
 MAX_IMAGE_GALLERY = 12
-HOME_ART_key = {"image_name": "", "image_url": "", "updated": False}
-COUNT = 0
 
 
 class Art(ndb.Model):
@@ -30,29 +27,32 @@ class Art(ndb.Model):
     score = ndb.FloatProperty()
 
     def _pre_put_hook(self):
-        global LIST_TAGS
+        list_tag = memcache.get("l_tags")
+        if list_tag is None:
+            list_tag = update_list_tags()
         for tag in self.tags:
-            if tag not in LIST_TAGS:
-                LIST_TAGS.append(tag)
+            if tag not in list_tag:
+                list_tag.append(tag)
+        memcache.set("l_tags", ["All"] + sorted(list_tag[1:]))
 
 
 def update_list_tags():
-    global LIST_TAG_UPDATED
-    global LIST_TAGS
-    if not LIST_TAG_UPDATED:
-        all_arts = Art.query()
-        for art in all_arts.fetch(6):
-            for tag in art.tags:
-                if tag not in LIST_TAGS:
-                    LIST_TAGS.append(tag)
-        LIST_TAG_UPDATED = True
+    all_arts = Art.query()
+    list_tag = ["All"]
+    for art in all_arts:
+        for tag in art.tags:
+            if tag not in list_tag:
+                list_tag.append(tag)
+    memcache.add("l_tags", list_tag)
+    return list_tag
 
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
-    def render_str(self, template, **params):
+    @staticmethod
+    def render_str(template, **params):
         t = jinja_env.get_template(template)
         return t.render(params)
 
@@ -64,12 +64,10 @@ class Handler(webapp2.RequestHandler):
         if user:
             self.response.headers['Content-Type'] = 'text/html'
             self.response.write('<!doctype html> <html> <p> Hello, ' + user.nickname() + "!You can <a href=\""
-                                 + "/logout" +
-                                 "\">sign out</a>.</p> </html>")
+                                + "/logout" + "\">sign out</a>.</p> </html>")
         else:
             self.response.write("<!doctype html> <html> <p> Hello,  you aren't log in yet !You can <a href=\""
-                                 + users.create_login_url(self.request.uri) +
-                                 "\">sign in</a>.</p> </html>")
+                                + users.create_login_url(self.request.uri) + "\">sign in</a>.</p> </html>")
 
 
 class HomePage(Handler):
@@ -77,18 +75,24 @@ class HomePage(Handler):
         self.render("home.html", image_name=image_name, image_url=image_url)
 
     def get(self):
-        global HOME_ART_key
-        global COUNT
-        COUNT += 1
-        if not HOME_ART_key["updated"] or COUNT == 4:
+        to_change = False
+        date_home_change = memcache.get("date_home_change")
+        if date_home_change is None:
+            to_change = True
+            memcache.set("date_home_change", date.today())
+        else:
+            if date_home_change < date.today():
+                to_change = True
+                memcache.set("date_home_change", date.today())
+        home_art_key = memcache.get("home_art_key")
+        if home_art_key is None or to_change:
             all_arts_keys = Art.query().fetch(keys_only=True)
             random_art_key = random.choice(all_arts_keys)
             random_art = random_art_key.get()
-            HOME_ART_key["image_name"] = random_art.title
-            HOME_ART_key["image_url"] = random_art.image_url
-            HOME_ART_key["updated"] = True
-            COUNT = 0
-        self.render_main(HOME_ART_key["image_name"], HOME_ART_key["image_url"])
+            home_art_key = dict()
+            home_art_key["image_name"] = random_art.title
+            home_art_key["image_url"] = random_art.image_url
+        self.render_main(home_art_key["image_name"], home_art_key["image_url"])
 
 
 class UploadFormHandler(Handler):
@@ -109,6 +113,7 @@ class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
         new_art.image_key = upload.key()
         new_art.image_url = get_serving_url(new_art.image_key)
         new_art.tags = self.request.get('image_tags').split()
+        new_art.score = 1
         new_art.put()
         self.redirect('/private/upload_form')
 
@@ -124,12 +129,12 @@ class ViewArtHandler(blobstore_handlers.BlobstoreDownloadHandler):
 def query_list_image(cursor, select_tag):
     curs = Cursor(urlsafe=cursor)
     if select_tag == "All" or select_tag == "":
-        all_arts, next_curs, more = Art.query().fetch_page(MAX_IMAGE_GALLERY, start_cursor=curs)
+        all_arts, next_curs, more = Art.query().order(-Art.score).fetch_page(MAX_IMAGE_GALLERY, start_cursor=curs)
     else:
         all_arts, next_curs, more = Art.query(Art.tags == select_tag).fetch_page(MAX_IMAGE_GALLERY, start_cursor=curs)
     list_image = []
     for art in all_arts:
-        list_image.append([art.image_url, art.key.urlsafe(), art.tags])
+        list_image.append([art.image_url, art.key.urlsafe(), art.tags, art.score])
     if more and next_curs:
         return list_image, more, next_curs.urlsafe()
     else:
@@ -138,7 +143,11 @@ def query_list_image(cursor, select_tag):
 
 class GalleryHandler(Handler):
     def render_main(self, list_image="", more=False, next_cursor="", tag=""):
-        self.render("gallery.html", list_image=list_image, more=more, next_cursor=next_cursor, private="", list_tags=LIST_TAGS, select_tag=tag)
+        list_tag = memcache.get("l_tags")
+        if list_tag is None:
+            list_tag = update_list_tags()
+        self.render("gallery.html", list_image=list_image, more=more, next_cursor=next_cursor, private="",
+                    list_tags=list_tag, select_tag=tag)
 
     def get(self):
         update_list_tags()
@@ -158,12 +167,18 @@ class ViewImageHandler(Handler):
         image = art.image_url
         image_name = art.title
         key_urlsafe = art.key.urlsafe()
+        art.score += 1
+        art.put()
         self.render_main(image, image_name, key_urlsafe)
 
 
 class PrivateGalleryHandler(Handler):
     def render_main(self, list_image="", more=False, next_cursor="", tag=""):
-        self.render("gallery.html", list_image=list_image, more=more, next_cursor=next_cursor, private="/private", list_tags=LIST_TAGS, select_tag=tag)
+        list_tag = memcache.get("l_tags")
+        if list_tag is None:
+            list_tag = update_list_tags()
+        self.render("gallery.html", list_image=list_image, more=more, next_cursor=next_cursor, private="/private",
+                    list_tags=list_tag, select_tag=tag)
 
     def get(self):
         update_list_tags()
@@ -177,7 +192,8 @@ class ModifyFormHandler(Handler):
     def render_main(self, url_key=""):
         art_key = ndb.Key(urlsafe=url_key)
         art = art_key.get()
-        self.render("modify_form.html", art_title=art.title, art_tags=art.tags, art_image=art.image_url, art_key=url_key)
+        self.render("modify_form.html", art_title=art.title, art_tags=art.tags, art_image=art.image_url, art_key=url_key
+                    , art_score=str(art.score))
 
     def get(self, url_key):
         self.render_main(url_key)
@@ -189,8 +205,9 @@ class ModifyHandler(Handler):
         art = art_key.get()
         art.title = self.request.get('image_name')
         art.tags = self.request.get('image_tags').split()
+        art.score = float(self.request.get('image_score'))
         art.put()
-        self.redirect('/private/modify_form/' + url_key)
+        self.redirect('/private/gallery')
 
 
 class ContactHandler(Handler):
@@ -225,6 +242,15 @@ class ExempleHandler(Handler):
         self.render_main()
 
 
+class ResetScoreHandler(Handler):
+    def get(self):
+        all_arts = Art.query()
+        for art in all_arts:
+            art.score = 1
+            art.put()
+        self.redirect("/private/gallery")
+
+
 app = webapp2.WSGIApplication([
     ('/', HomePage),
     ('/view_art/([^/]+)?', ViewArtHandler),
@@ -238,5 +264,6 @@ app = webapp2.WSGIApplication([
     ('/private/modify_form/([^/]+)?', ModifyFormHandler),
     ('/private/modify/([^/]+)?', ModifyHandler),
     ('/private/upload_form', UploadFormHandler),
+    ('/private/reset_score', ResetScoreHandler),
     ('/upload', UploadHandler),
 ], debug=True)
